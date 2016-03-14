dc171d17b310034cd7a950557f4e4dec82f2c152
afl1 <afl2001@gmail.com>
2016-03-14 14:09:19 +0100

    Update vpnc-script.js
    Following fixies and new features in update:
    - fixed routing when host ip address is first from subnet;
    - added routes for DNS a WINS servers;
    - added VPN domain to DNS suffix list;
    - added connection-specific DNS suffix for vPN tunnel interface;
    - added complete clean-up after disconnect;

--- nsis/vpnc-script.js
+++ nsis/vpnc-script.js
@@ -10,6 +10,8 @@
 var fs = WScript.CreateObject("Scripting.FileSystemObject");
 var tmpdir = fs.GetSpecialFolder(2)+"\\";
 var log = fs.OpenTextFile(tmpdir + "vpnc.log", 8, true);
+var WshShell = WScript.CreateObject ("WScript.Shell");
+var searchList = WshShell.RegRead ("HKLM\\System\\CurrentControlSet\\Services\\TCPIP\\Parameters\\SearchList");
 
 function echo(msg)
 {
@@ -17,9 +19,9 @@ function echo(msg)
 }
 
 function exec(cmd)
- {
+{
        var s = "";
-       log.WriteLine("executing: " + cmd);
+       echo("executing: " + cmd);
 
        if (fs.FileExists(tmpdir + "vpnc.out")) {
                fs.DeleteFile(tmpdir + "vpnc.out");
@@ -38,10 +40,10 @@ function exec(cmd)
        }
        return s;
 }
-  
+
 function getDefaultGateway()
 {
-	if (exec("route print").match(/0\.0\.0\.0 *(0|128)\.0\.0\.0 *([0-9\.]*)/)) {
+	if (exec("route print -4").match(/0\.0\.0\.0 *(0|128)\.0\.0\.0 *([0-9\.]*)/)) {
 		return (RegExp.$2);
 	}
 	return ("");
@@ -56,19 +58,82 @@ function getInterfaceId(ifname)
 		return (RegExp.$1);
 	}
 	echo("interface \"" + ifname + "\" not found");
-	return ("");
+	return (ifname);
 }
 
 function waitForInterface() {
 	var if_route = new RegExp(env("INTERNAL_IP4_ADDRESS") + " *255.255.255.255");
-	for (var i = 0; i < 5; i++) {
+	for (var i = 0; i < 7; i++) {
 		echo("Waiting for interface to come up...");
-		WScript.Sleep(2000);
-		if (exec("route print").match(if_route)) {
+		if (exec("route print -4 " + env("INTERNAL_IP4_ADDRESS")).match(if_route)) {
 			return true;
 		}
+		WScript.Sleep(1000);
 	}
 	return false;
+}
+function addRoute(ip, mask, gw ) {
+	var if_route = new RegExp("(" + ip + " *"+ mask + " *" + gw + ")", "m");
+
+	exec("route add " + ip  + " mask " + mask + " " + gw);
+	for (var i = 0; i < 15; i++) {
+		echo("Waiting for route to come up...");
+		if (exec("route print -4").match(if_route)) {
+			return true;
+		}
+		WScript.Sleep(1000);
+	}
+	echo("Route " + ip + " mask " + mask + " does not seem to be up.");
+	return false;
+}
+var HKLM = 0x80000002; 
+
+function regGetUUID( strRegPath, inf) { 
+  try { 
+    var aUUID = null; 
+    var bUUID = null; 
+    var objLocator     = new ActiveXObject("WbemScripting.SWbemLocator"); 
+    var objService     = objLocator.ConnectServer(".", "root\\default"); 
+    var objReg         = objService.Get("StdRegProv"); 
+    var objMethod      = objReg.Methods_.Item("EnumKey"); 
+    var objInParam     = objMethod.InParameters.SpawnInstance_(); 
+    objInParam.hDefKey = HKLM; 
+    objInParam.sSubKeyName = strRegPath; 
+    var objOutParam = objReg.ExecMethod_(objMethod.Name, objInParam); 
+
+    if (objOutParam.ReturnValue ==0) 
+    { 
+        aUUID = (objOutParam.sNames != null) ? objOutParam.sNames.toArray(): null; 
+		    for (var idx=0; idx < aUUID.length;idx++) 
+        { 
+          objInParam.sSubKeyName = strRegPath + "\\" + aUUID[idx]
+          objOutParam = objReg.ExecMethod_(objMethod.Name, objInParam)
+          if (objOutParam.ReturnValue == 0){
+          	if (objOutParam.sNames != null) {
+          		bUUID = objOutParam.sNames.toArray(); 
+          		for (var j=0; j < bUUID.length;j++){ 
+         	  		try{ 
+         					var strReg = WshShell.RegRead ("HKLM\\System\\CurrentControlSet\\Control\\Network\\" 
+         						+ aUUID[idx] + "\\" + bUUID[j] + "\\Connection\\Name");
+									if (strReg != null){
+										if (inf == strReg){
+											return (bUUID[j]);
+										}
+									} 
+								}
+  							catch(e) {  
+  							}
+							}
+  					}
+  				}	
+  			} 
+				return ("");
+    } 
+    return (""); 
+  } 
+  catch(e) {  
+    return (""); 
+  } 
 }
 
 // --------------------------------------------------------------
@@ -85,11 +150,23 @@ var env = ws.Environment("Process");
 // 1 - As a 0.0.0.0/0 route with a lower metric than the default route
 // 2 - As 0.0.0.0/1 + 128.0.0.0/1 routes (override the default route cleanly)
 var REDIRECT_GATEWAY_METHOD = 0;
+var tundevid = getInterfaceId(env("TUNDEV"));
+var tunReg 	= regGetUUID ("System\\CurrentControlSet\\Control\\Network\\",env("TUNDEV"));
+
+function getInterfaceId(ifname)
+{
+	var rx_ifid = new RegExp("^ *([0-9]+) *.*" + ifname + "$", "m")
+	if (exec("netsh interface ip show interfaces").search(rx_ifid)) {
+		return (RegExp.$1);
+	}
+	return ("");
+}
 
 switch (env("reason")) {
 case "pre-init":
 	break;
 case "connect":
+  echo("================== Connect ============");
 	var gw = getDefaultGateway();
 	var address_array = env("INTERNAL_IP4_ADDRESS").split(".");
 	var netmask_array = env("INTERNAL_IP4_NETMASK").split(".");
@@ -100,19 +177,29 @@ case "connect":
 		address_array[2] & netmask_array[2],
 		(address_array[3] & netmask_array[3]) + 1
 	);
+	if (internal_gw_array[3] == address_array[3]){
+		internal_gw_array[3] = (address_array[3] & netmask_array[3] ) + 2
+  }
 	var internal_gw = internal_gw_array.join(".");
-	var tundevid = getInterfaceId(env("TUNDEV"))
-	
-	echo("VPN Gateway     : " + env("VPNGATEWAY"));
+  if (searchList.search(env("CISCO_DEF_DOMAIN")) != 0) {
+  	searchList = env("CISCO_DEF_DOMAIN") + "," + searchList;
+		WshShell.RegWrite ("HKLM\\System\\CurrentControlSet\\Services\\TCPIP\\Parameters\\SearchList", 
+		   searchList, "REG_SZ");
+		exec("ipconfig /registerdns");
+  }	
+  if (tunReg != "") {
+  	WshShell.RegWrite ("HKLM\\System\\CurrentControlSet\\Services\\TCPIP\\Parameters\\Interfaces\\" + tunReg + "\\Domain",
+		   env("CISCO_DEF_DOMAIN"), "REG_SZ");
+	}
+	echo("DNS suffix search: " + searchList);
+	echo("VPN Gateway: " + env("VPNGATEWAY"));
 	echo("Internal Address: " + env("INTERNAL_IP4_ADDRESS"));
 	echo("Internal Netmask: " + env("INTERNAL_IP4_NETMASK"));
 	echo("Internal Gateway: " + internal_gw);
-	echo("Interface name  : \"" + env("TUNDEV") + "\"");
+	echo("Interface name  : \"" + env("TUNDEV") + "\" " + tunReg);
 	echo("Interface id    : " + tundevid);
-
 	// Add direct route for the VPN gateway to avoid routing loops
-	exec("route add " + env("VPNGATEWAY") +
-            " mask 255.255.255.255 " + gw);
+	addRoute(env("VPNGATEWAY"), "255.255.255.255", gw);
 
 	if (env("INTERNAL_IP4_MTU")) {
 	    echo("MTU: " + env("INTERNAL_IP4_MTU"));
@@ -139,11 +226,16 @@ case "connect":
 		exec("netsh interface ip set address \"" + tundevid + "\" static " +
 			env("INTERNAL_IP4_ADDRESS") + " " + env("INTERNAL_IP4_NETMASK") + " " + internal_gw + " 1");
 	}
+		// Waiting for the interface to be configured before to add routes
+	if (!waitForInterface()) {
+			echo("Interface does not seem to be up.");
+	}
 
     if (env("INTERNAL_IP4_NBNS")) {
 		var wins = env("INTERNAL_IP4_NBNS").split(/ /);
 		for (var i = 0; i < wins.length; i++) {
-	                exec("netsh interface ip add wins \"" +
+		    addRoute(wins[i], "255.255.255.255", internal_gw);
+	      exec("netsh interface ipv4 add wins \"" +
 			    tundevid + "\" " + wins[i]
 			    + " index=" + (i+1));
 		}
@@ -152,34 +244,21 @@ case "connect":
     if (env("INTERNAL_IP4_DNS")) {
 		var dns = env("INTERNAL_IP4_DNS").split(/ /);
 		for (var i = 0; i < dns.length; i++) {
-	                exec("netsh interface ip add dns \"" +
-			    tundevid + "\" " + dns[i]
+		    addRoute(dns[i], "255.255.255.255", internal_gw);
+	      exec("netsh interface ipv4 add dns \"" +
+			    tundevid + "\" " + dns[i] 
 			    + " index=" + (i+1));
 		}
 	}
-	echo("done.");
-
-	// Add internal network routes
-    echo("Configuring Legacy IP networks:");
-    if (env("CISCO_SPLIT_INC")) {
-		// Waiting for the interface to be configured before to add routes
-		if (!waitForInterface()) {
-			echo("Interface does not seem to be up.");
-		}
-		
+ 
+    if (env("CISCO_SPLIT_INC")) {   	
+	// Add split tunnel network routes
 		for (var i = 0 ; i < parseInt(env("CISCO_SPLIT_INC")); i++) {
 			var network = env("CISCO_SPLIT_INC_" + i + "_ADDR");
 			var netmask = env("CISCO_SPLIT_INC_" + i + "_MASK");
-			var netmasklen = env("CISCO_SPLIT_INC_" + i +
-					 "_MASKLEN");
-			exec("route add " + network + " mask " + netmask +
-			     " " + internal_gw);
+			addRoute(network, netmask, internal_gw);
 		}
 	} else if (REDIRECT_GATEWAY_METHOD > 0) {
-		// Waiting for the interface to be configured before to add routes
-		if (!waitForInterface()) {
-			echo("Interface does not seem to be up.");
-		}
 		
 		if (REDIRECT_GATEWAY_METHOD == 1) {
 			exec("route add 0.0.0.0 mask 0.0.0.0 " + internal_gw + " metric 1");
@@ -188,10 +267,10 @@ case "connect":
 			exec("route add 128.0.0.0 mask 128.0.0.0 " + internal_gw);
 		}
 	}
-	echo("Route configuration done.");
+	echo("IPv4 route configuration done.");
 
-        if (env("INTERNAL_IP6_ADDRESS")) {
-		echo("Configuring \"" + tundevid + "\" interface for IPv6...");
+  if (env("INTERNAL_IP6_ADDRESS")) {
+		echo("Configuring \"" +  tundevid + "\" interface for IPv6...");
 
 		exec("netsh interface ipv6 set address \"" + tundevid + "\" " +
 		    env("INTERNAL_IP6_ADDRESS") + " store=active");
@@ -199,7 +278,7 @@ case "connect":
 		echo("done.");
 
 		// Add internal network routes
-	        echo("Configuring Legacy IP networks:");
+	  echo("Configuring IPv6 networks:");
 	        if (env("INTERNAL_IP6_NETMASK") && !env("INTERNAL_IP6_NETMASK").match("/128$")) {
 			exec("netsh interface ipv6 add route " + env("INTERNAL_IP6_NETMASK") +
 			    " \"" + tundevid + "\" fe80::8 store=active");
@@ -226,10 +305,96 @@ case "connect":
 		echo(env("CISCO_BANNER"));
 		echo("--------------------------------------------------");
 	}
+	echo("done.");
 	break;
 case "disconnect":
+  echo("================== Disconnect ============");
 	// Delete direct route for the VPN gateway
+		for (var i = 0 ; i < parseInt(env("CISCO_SPLIT_INC")); i++) {
+			var network = env("CISCO_SPLIT_INC_" + i + "_ADDR");
+			var netmask = env("CISCO_SPLIT_INC_" + i + "_MASK");
+			exec("route delete " + network + " mask " + netmask );
+		}
+  if (env("INTERNAL_IP4_NBNS")) {
+		var wins = env("INTERNAL_IP4_NBNS").split(/ /);
+		for (var i = 0; i < wins.length; i++) {
+	                exec("netsh interface ipv4 delete wins \"" +
+			    tundevid + "\" " + wins[i]);
+	  							exec("route delete " + wins[i] + " mask 255.255.255.255");
+	}
+	}
+
+  if (env("INTERNAL_IP4_DNS")) {
+		var dns = env("INTERNAL_IP4_DNS").split(/ /);
+		for (var i = 0; i < dns.length; i++) {
+	                exec("netsh interface ipv4 delete dns \"" +
+			    tundevid + "\" " + dns[i] );
+	  							exec("route delete " + dns[i] + " mask 255.255.255.255");
+		}
+	}
+  searchList = WshShell.RegRead ("HKLM\\System\\CurrentControlSet\\Services\\TCPIP\\Parameters\\SearchList");
+	var searchList_array = searchList.split(",");
+	for (i = searchList_array.length - 1; i >= 0; i--){
+		if (searchList_array[i] == env("CISCO_DEF_DOMAIN")) {
+			searchList_array.splice(i, 1);
+		}
+	}
+	searchList = searchList_array.join(",");
+  if (WshShell.RegRead ("HKLM\\SYSTEM\\ControlSet001\\services\\Tcpip\\Parameters\\Domain") == env("CISCO_DEF_DOMAIN")){
+  	if (searchList == ""){
+  		searchList = env("CISCO_DEF_DOMAIN");
+  	} else {
+   		searchList = env("CISCO_DEF_DOMAIN") + "," + searchList;
+  	}
+  }
+  WshShell.RegWrite ("HKLM\\System\\CurrentControlSet\\Services\\TCPIP\\Parameters\\SearchList", searchList, "REG_SZ");
+	echo("DNS suffix search: " + searchList);
+  if (tunReg != "") {
+  	WshShell.RegWrite ("HKLM\\System\\CurrentControlSet\\Services\\TCPIP\\Parameters\\Interfaces\\" + tunReg + "\\Domain",
+		   "", "REG_SZ");
+	}
+	exec("ipconfig /registerdns");
 	exec("route delete " + env("VPNGATEWAY") + " mask 255.255.255.255");
+	
+	if (env("CISCO_SPLIT_INC") || REDIRECT_GATEWAY_METHOD > 0) {
+		exec("netsh interface ip delete address \"" + tundevid + "\" " + 
+			env("INTERNAL_IP4_ADDRESS"));
+	} else {
+		exec("netsh interface ip delete address \"" + tundevid + "\" " +
+			env("INTERNAL_IP4_ADDRESS") + " " + internal_gw);
+	}
+	if (!env("CISCO_SPLIT_INC") && REDIRECT_GATEWAY_METHOD != 2) {
+		exec("netsh interface ip delete interface \"" + tundevid + "\"");
+	}
+
+	//  ======================== IPv6 ===================================
+  if (env("INTERNAL_IP6_ADDRESS")) {
+		exec("netsh interface ipv6 delete address \"" + tundevid + "\" " +
+		    env("INTERNAL_IP6_ADDRESS"));
+
+			// Delete internal network routes
+	        echo("Configuring Legacy IP networks:");
+	        if (env("INTERNAL_IP6_NETMASK") && !env("INTERNAL_IP6_NETMASK").match("/128$")) {
+			exec("netsh interface ipv6 delete route " + env("INTERNAL_IP6_NETMASK") +
+			    " \"" + tundevid + "\"");
+		}
+
+	        if (env("CISCO_IPV6_SPLIT_INC")) {
+			for (var i = 0 ; i < parseInt(env("CISCO_IPV6_SPLIT_INC")); i++) {
+				var network = env("CISCO_IPV6_SPLIT_INC_" + i + "_ADDR");
+				var netmasklen = env("CISCO_SPLIT_INC_" + i +
+						 "_MASKLEN");
+				exec("netsh interface ipv6 delete route " + network + "/" +
+				    netmasklen + " \"" + tundevid + "\"");
+			}
+		} else {
+			echo("Deleting default IPv6 route through VPN.");
+			exec("netsh interface ipv6 delete route 2000::/3 \"" + tundevid +
+			    "\"");
+		}
+		echo("IPv6 route configuration done.");
+	}
+	echo("done.");
 }
 
 log.Close();
